f49f8a0f0b414f4245d25bfbc465f7792cc42ed5
linuxer <919313096@qq.com>
2018-06-09 14:06:07 +0800

    增加game_show_cells文件，用来显示细胞信息

---
+++ game_show_cells.py
@@ -0,0 +1,17 @@
+# 该类用于将细胞计算结果显示出来
+from pygame import draw
+
+
+class ShowCells():
+    def show_cells(window, cell, info):
+
+        game_scale = info['game_size'] / info['grid_size']
+        for value_index in range(len(cell)):
+
+            # Change index to pos
+            x, y = PlotCell.index_to_index(info, value_index)
+
+            value = cell[value_index]
+            if value: 
+                draw.rect(window, info['live_colour'],
+                          (x * game_scale, y * game_scale, int(game_scale), int(game_scale)))
