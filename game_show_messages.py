a23fbb8b92f307eaac980add10bbf0af81bf920d
linuxer <919313096@qq.com>
2018-06-09 14:00:00 +0800

    增加game_show_messages文件，用来显示鼠标点击位置的效果

---
+++ game_show_messages.py
@@ -0,0 +1,17 @@
+# 该类用来显示游戏信息
+from pygame import font
+
+
+class ShowMessages():
+    def show_messages(window, info, messages):
+
+        max_height = 30
+        margin = 10
+
+        f = font.SysFont(None, int(max_height))
+
+        for index in range(len(messages)):
+            surf = f.render(messages[index], 0, (255, 255, 255))
+
+            y = (margin + max_height) * index + margin
+            window.blit(surf, (margin, y))
