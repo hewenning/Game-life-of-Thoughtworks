51a932f031066dc4b341f0ab597d52bd799a265b
linuxer <919313096@qq.com>
2018-06-09 13:41:25 +0800

    增加get_pos_on_game函数，用来处理鼠标在游戏上的点击信息

--- game_cell_change.py
+++ game_cell_change.py
@@ -13,12 +13,28 @@ class ChangeCell():
         if mouse.get_pressed()[2]:
             change = False
 
-        if not change == None:
-            pos = get_pos_on_game(info)
+        if not change is None:
+            pos = ChangeCell.get_pos_on_game(info)
 
             # 判断这个细胞是否无效
             if pos[0] >= 0 and pos[1] >= 0 and pos[0] <= info['cell_size'] and pos[1] <= info['cell_size']:
-                index = pos_to_index(info, pos)
+                index = PlotCell.pos_to_index(info, pos)
                 cell[index] = change
 
         return cell
+
+    def get_pos_on_game(info):
+
+        pos = list(mouse.get_pos())
+        game_scale = info['game_size'] / info['grid_size']
+
+        pos[0] -= (info['window_size'][0] - info['game_size']) / 2
+        pos[1] -= (info['window_size'][1] - info['game_size']) / 2
+
+        pos[0] /= game_scale
+        pos[1] /= game_scale
+
+        pos[0] = int(pos[0])
+        pos[1] = int(pos[1])
+
+        return pos
